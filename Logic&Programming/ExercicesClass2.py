9342ab67ca443e3b76e56837754ba4a6a6bba67f
Luane Barcellos <luanebsg@gmail.com>
2024-02-24 19:42:06 -0300

    aula2parte5

--- Logic&Programming/ExercicesClass2.py
+++ Logic&Programming/ExercicesClass2.py
@@ -33,3 +33,33 @@ n = float(input())
 print('Quadrado do valor', n**2)
 print('Cubo do valor', n**3)
 print('Valor na quarta', n**4)
+
+# Fahrenheit
+
+print('Informe o valor de Fahrenheit:')
+f = float(input())
+c = 5/9*(f-32)
+print('Convertido em Celcius:', c)
+
+# Conversão de segundos decomposto em hora, minuto e segundo.
+
+print('Insira os segundos:')
+seg = int(input())
+h = seg//3600
+m = (seg%3600)//60
+s = seg%60
+print('Decomposição em hora:', h)
+print('Decomposição em minuto:', m)
+print('Decomposição em segundo:', s)
+
+# Leitura da ordem inversa do input
+
+print('Informe um valor inteiro de 4 dígitos:')
+value = int(input())
+m = value//1000
+resto = value % 1000
+c = resto // 100
+resto = resto % 100
+d = resto // 10
+u = resto % 10
+print('Valor ao contrário:', u*1000 + d*100 + c*10 + m )
