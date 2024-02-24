879bb81ac89fbf5d0228752b6fd552d3225f1f67
Luane Barcellos <luanebsg@gmail.com>
2024-02-24 17:43:24 -0300

    Class2part4

---
+++ Logic&Programming/ExercicesClass2.py
@@ -0,0 +1,35 @@
+# Exercício 1
+
+import math
+print('Informe o Valor do raio: ')
+raio = float(input())
+volume = 4/3 * math.pi * raio ** 3
+print('Volume:', volume)
+
+# Exercício 2
+
+import math
+print('Informe o valor do raio:')
+raio = float(input())
+a = 4 * math.pi * raio ** 2
+print('Área:', a)
+
+#Como eu fiz, ex3
+
+import math
+print('Informe o valor de n:')
+n = float(input())
+p0 = n**2
+p1 = n**3
+p2 = n**4
+print('Resultado de n**2:', p0)
+print('Resultado de n**3:', p1)
+print('Resultado de n**4:', p2)
+
+# Como a prof fez
+
+print('Informe um valor:')
+n = float(input())
+print('Quadrado do valor', n**2)
+print('Cubo do valor', n**3)
+print('Valor na quarta', n**4)
